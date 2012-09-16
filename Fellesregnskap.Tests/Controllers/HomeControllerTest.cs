4500dba62d33eea9ba1dd77533a0cd0df5cc590a
Jonas <sirjonas@gmail.com>
2012-09-16 16:13:58 +0200

    Slettet en test

--- Fellesregnskap.Tests/Controllers/HomeControllerTest.cs
+++ Fellesregnskap.Tests/Controllers/HomeControllerTest.cs
@@ -25,17 +25,5 @@ namespace Fellesregnskap.Tests.Controllers
             Assert.AreEqual("Welcome to ASP.NET MVC!", result.ViewBag.Message);
         }
 
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
     }
 }
