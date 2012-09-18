6e1859f3ec4d8bb08b2b67e5b95e533b57976b14
Øyvin Richardsen <desarc00@gmail.com>
2012-09-18 18:10:25 +0200

    fix

--- Fellesregnskap.Tests/App_Code/MongoAccesorTest.cs
+++ Fellesregnskap.Tests/App_Code/MongoAccesorTest.cs
@@ -27,9 +27,9 @@ namespace Fellesregnskap.Tests.App_Code
         [TestMethod]
         public void TestGetReceiptsMonth()
         {
-            var receipts1 = MongoAccessor.GetReceiptsByMonth(14);
+            var receipts1 = MongoAccessor.GetReceiptsByMonth(14, DateTime.Now.Year);
             Assert.AreEqual(2, receipts1.Count);
-            var receipts2 = MongoAccessor.GetReceiptsByMonth(DateTime.Now.Month);
+            var receipts2 = MongoAccessor.GetReceiptsByMonth(DateTime.Now.Month, DateTime.Now.Year);
             Assert.AreEqual(3, receipts2.Count);
         }
 
@@ -37,13 +37,13 @@ namespace Fellesregnskap.Tests.App_Code
         public void TestGetAllReceiptsForParticipantByMonth()
         {
             var participant1 = MongoAccessor.GetParticipant("Arnulf");
-            var receipts1 = MongoAccessor.GetAllReceiptsForParticipantByMonth(participant1, 14);
+            var receipts1 = MongoAccessor.GetAllReceiptsForParticipantByMonth(participant1, 14, DateTime.Now.Year);
             Assert.AreEqual(1, receipts1.Count);
             var participant2 = MongoAccessor.GetParticipant("Annie");
-            var receipts2 = MongoAccessor.GetAllReceiptsForParticipantByMonth(participant2, DateTime.Now.Month);
+            var receipts2 = MongoAccessor.GetAllReceiptsForParticipantByMonth(participant2, DateTime.Now.Month, DateTime.Now.Year);
             Assert.AreEqual(0, receipts2.Count);
             var participant3 = MongoAccessor.GetParticipant("Abed");
-            var receipts3 = MongoAccessor.GetAllReceiptsForParticipantByMonth(participant3, DateTime.Now.Month);
+            var receipts3 = MongoAccessor.GetAllReceiptsForParticipantByMonth(participant3, DateTime.Now.Month, DateTime.Now.Year);
             Assert.AreEqual(1, receipts3.Count);
         }
 
@@ -51,11 +51,11 @@ namespace Fellesregnskap.Tests.App_Code
         public void TestGetAllReceiptsForPayerByMonth()
         {
             var payer1 = MongoAccessor.GetParticipant("Arnulf");
-            var receipts1 = MongoAccessor.GetAllReceiptsForPayerByMonth(payer1, 14);
+            var receipts1 = MongoAccessor.GetAllReceiptsForPayerByMonth(payer1, 14, DateTime.Now.Year);
             Assert.AreEqual(1, receipts1.Count);
-            var receipts2 = MongoAccessor.GetAllReceiptsForPayerByMonth(payer1, DateTime.Now.Month);
+            var receipts2 = MongoAccessor.GetAllReceiptsForPayerByMonth(payer1, DateTime.Now.Month, DateTime.Now.Year);
             Assert.AreEqual(2, receipts2.Count);
-            var receipts3 = MongoAccessor.GetAllReceiptsForPayerByMonth(payer1, 15);
+            var receipts3 = MongoAccessor.GetAllReceiptsForPayerByMonth(payer1, 15, DateTime.Now.Year);
             Assert.AreEqual(0, receipts3.Count);
         }
     }
--- Fellesregnskap/Controllers/HomeController.cs
+++ Fellesregnskap/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace Fellesregnskap.Controllers
     {
         public ActionResult Index()
         {
+           
             return View();
         }
 
