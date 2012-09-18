69451ff87aeaf6a1c8aff3b4e7ee4bb2ec2d56a6
Jonas <sirjonas@gmail.com>
2012-09-18 17:37:18 +0200

    La til ny viewmodel

---
+++ Fellesregnskap/ViewModels/ParticipantBalance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Fellesregnskap.Models.Home;
+
+namespace Fellesregnskap.ViewModels
+{
+    public class ParticipantBalance
+    {
+        public double Balance { get; set; }
+        public Participant Participant { get; set; }
+    }
+}
