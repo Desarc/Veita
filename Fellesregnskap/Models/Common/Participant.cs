f6fd0761a1a2dba2d6c3b62df6347cc6ce3f3770
Jonas <sirjonas@gmail.com>
2012-09-18 17:41:59 +0200

    Adding moved files

---
+++ Fellesregnskap/Models/Common/Participant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fellesregnskap.Models.Common
+{
+    public class Participant
+    {
+        public ObjectId Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+    }
+}
